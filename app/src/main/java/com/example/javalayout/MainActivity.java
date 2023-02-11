db2e6f9e7a5a355d711b7541d1505a73e3140d42
Adam <adam.aji2007@gmail.com>
2023-02-11 07:25:13 +0700

    feat : [AD] add-relativeLayout

--- app/src/main/java/com/example/javalayout/MainActivity.java
+++ app/src/main/java/com/example/javalayout/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.javalayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText btnUsername;
+    EditText btnPassword;
+    Button btnLogin;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // konekkan semua komponen dengan xml nya
+
+        btnUsername = (EditText) findViewById(R.id.btnUsername);
+        btnPassword = (EditText) findViewById(R.id.btnPassword);
+        btnLogin = (Button) findViewById(R.id.btnlogins);
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                String username = btnUsername.getText().toString();
+                String password = btnPassword.getText().toString();
+                if (username.equalsIgnoreCase("admin") &&
+                        password.equalsIgnoreCase("admin")){
+                    startActivity(new Intent(MainActivity.this,MainActivity2.class));
+                }else{
+                    Toast.makeText(getApplicationContext(), "Gagal Login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
